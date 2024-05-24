bf0946bb62f1b0ef21a72873ca5097a540cbd247
kumar amit <amit@moneynmore.com>
2024-05-24 07:01:11 +0530

    Grappelli Implementation

--- cmsservice/settings.py
+++ cmsservice/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'grappelli',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -121,7 +122,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-
+STATIC_ROOT = Path(BASE_DIR, 'static')
 STATIC_URL = 'static/'
 
 # Default primary key field type
